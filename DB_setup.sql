1dbc228b363ed75287ecd0887b604752f7149b37
PRITHU VERMA <142580031+PrithuVerma@users.noreply.github.com>
2026-04-18 14:51:50 +0530

    Add files via upload

---
+++ DB_setup.sql
@@ -0,0 +1,14 @@
+-- Database: Credit_Risk
+
+-- DROP DATABASE IF EXISTS "Credit_Risk";
+
+CREATE DATABASE "Credit_Risk"
+    WITH
+    OWNER = prithu
+    ENCODING = 'UTF8'
+    LC_COLLATE = 'en_US.UTF-8'
+    LC_CTYPE = 'en_US.UTF-8'
+    LOCALE_PROVIDER = 'libc'
+    TABLESPACE = pg_default
+    CONNECTION LIMIT = -1
+    IS_TEMPLATE = False;
